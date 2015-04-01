9a49fba0db0a3817544e879a6f65ca9674a84bd4
Albert Engelbrecht <dropped.on.japan@gmail.com>
2015-04-01 09:55:23 -0500

    Finished tests, fixed bug.

--- test/cronCalc.test.js
+++ test/cronCalc.test.js
@@ -18,4 +18,18 @@ m.describe('nextRun()', function() {
 
     done();
   });
+
+  m.it('Should calculate correct date if supplied a date', function(done) {
+    var d = cronCalc.nextRun('0 4 * * *', '2015-04-01T13:52:00.384Z');
+    d.should.be.ok.and.be.a.Date;
+
+    var shouldBe = new Date('2015-04-01T13:52:00.384Z');
+    shouldBe.setDate(shouldBe.getDate() + 1);
+    shouldBe.setHours(4,0,0,999);
+
+    d.getDate().should.equal(shouldBe.getDate());
+    d.getHours().should.equal(shouldBe.getHours());
+
+    done();
+  });
 });
